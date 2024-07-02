880da5247fdb8ace32f61b5dced9a4289d02a792
Ryan Earwood <ear24003@byui.edu>
2024-07-02 16:11:50 -0400

    mobile optimized

---
+++ company.js
@@ -0,0 +1,11 @@
+const lastModified = new Date(document.lastModified);
+
+    // Format the date as desired (e.g., "Month Day, Year")
+    const formattedDate = lastModified.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    });
+
+    // Update the content of the element with the ID "lastModified" with the formatted date
+    document.getElementById("lastModified").textContent = formattedDate;
